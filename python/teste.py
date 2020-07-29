bd6c117280a0daab966ec6ae3c5e5aa04bfd6318
elleeenc <ellen.viana00@gmail.com>
2020-07-29 18:02:57 -0300

    arquivo python

---
+++ python/teste.py
@@ -0,0 +1,2 @@
+import urllib.request
+urllib.request.urlretrieve('http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar', "Padrao_TISS_Componente_Organizacional_202006.pdf")
