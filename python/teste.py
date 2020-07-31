efc747e54621f32c87229b43e989f71654be2fc1
elleeenc <ellen.viana00@gmail.com>
2020-07-31 18:46:59 -0300

    convertendo as tabelas extraidas em pdf para o formato excel

--- python/teste.py
+++ python/teste.py
@@ -1,10 +1,12 @@
 import urllib.request
 import tabula 
-# import pandas as pd
+import pandas as pd
 
 urllib.request.urlretrieve('http://www.ans.gov.br/images/stories/Plano_de_saude_e_Operadoras/tiss/Padrao_tiss/tiss3/Padrao_TISS_Componente_Organizacional_202006.pdf', "Padrao_TISS_Componente_Organizacional_202006.pdf")
 pdf_file = open('Padrao_TISS_Componente_Organizacional_202006.pdf', 'rb')
 
 path = '/Users/mrock/Desktop/python/Padrao_TISS_Componente_Organizacional_202006.pdf'
-df = tabula.read_pdf(path, pages = '81,82,83,84,85,86,87,88', lattice  = False)
+df = pd.DataFrame(tabula.read_pdf(path, pages = '81,82,83,84,85,86,87,88', lattice  = False))
 print(df)
+
+df.to_csv('/Users/mrock/Desktop/python/teste.csv', index = True, encoding="utf-8")
