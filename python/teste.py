eb895f5a1e434974516b3375fee92ea0b38d76f2
elleeenc <ellen.viana00@gmail.com>
2020-08-04 17:58:00 -0300

    tabelas extraidas e convertidas para excel

--- python/teste.py
+++ python/teste.py
@@ -1,12 +1,12 @@
 import urllib.request
 import tabula 
-import pandas as pd
 
 urllib.request.urlretrieve('http://www.ans.gov.br/images/stories/Plano_de_saude_e_Operadoras/tiss/Padrao_tiss/tiss3/Padrao_TISS_Componente_Organizacional_202006.pdf', "Padrao_TISS_Componente_Organizacional_202006.pdf")
-pdf_file = open('Padrao_TISS_Componente_Organizacional_202006.pdf', 'rb')
+pdf_file = open('Padrao_TISS_Componente_Organizacional_202006.pdf','rb')
 
-path = '/Users/mrock/Desktop/python/Padrao_TISS_Componente_Organizacional_202006.pdf'
-df = pd.DataFrame(tabula.read_pdf(path, pages = '81,82,83,84,85,86,87,88', lattice  = False))
+path ='/Users/mrock/Desktop/python/Padrao_TISS_Componente_Organizacional_202006.pdf'
+df = (tabula.io.read_pdf(path,pages = '81-88', encoding = 'utf-8'))
 print(df)
 
-df.to_csv('/Users/mrock/Desktop/python/teste.csv', index = True, encoding="utf-8")
+tabula.convert_into(path,output_path= 'tabela.csv', output_format = 'csv', pages = '81-88')
+
